0b5f6b3bf529ba747ad4e06a521c5fc8fb72cae0
tj <tj.schwartz2000@gmail.com>
2024-09-30 10:52:53 -0400

    Add matrix slicing so ensure that multiplying matrices is optimized.

--- MatrixLibrary/MatrixLibrary.cpp
+++ MatrixLibrary/MatrixLibrary.cpp
@@ -1,14 +1,105 @@
 #include "pch.h"
 #include "MatrixLibrary.h"
 
-UINT_32* matrix_multiply_2d(int nrows, int ncols , UINT_32* n, int mrows, int mcols, UINT_32* m)
+double* matrix_multiply_2d_worker(int nrows, int ncols, double* n, int mrows, int mcols, double* m, double* ret);
+double* generate_matrix_slice_vertical(int nrows, int ncols, double* n, int scols, int startCol);
+double* generate_matrix_slice_horizontal(int mrows, int mcols, double* m, int srows, int startRow);
+
+
+double* matrix_multiply_2d(int nrows, int ncols, double* n, int mrows, int mcols, double* m)
 {
 	if (ncols != mrows) return 0;
 
 	int finalNumRows = nrows;
 	int finalNumCols = mcols;
 	int numElements = finalNumRows * finalNumCols;
-	UINT_32* ret = new UINT_32[numElements];
+	double* ret = new double[numElements];
+	for (int i = 0; i < numElements; i++) { *(ret + i) = 0; }//Default initialize ret to zero
+	//The goal is to create a bunch of matrix slices that all can hit the optimized case where there's only one loop
+	//This will make matrix multiplication as fast as I could imagine here
+	if (ncols > 5 && numElements > 100 * 100) 
+	{
+
+		int numSlices = ncols / 5;
+		int numColsOnLast = ncols % 5;
+		std::thread* threads = new std::thread[numSlices];
+		int startIndex = 0;
+		for (int i = 0; i < numSlices; i++)
+		{
+			int sliceSize = 5;
+			if (i + 1 == numSlices) //ncols = mrows so this works for n and m
+			{
+				sliceSize = numColsOnLast;
+			}
+			double* nslice = generate_matrix_slice_vertical(nrows, ncols, n, sliceSize, i * 5); //Generate a vertical slice of n
+			double* mslice = generate_matrix_slice_horizontal(mrows, mcols, m, sliceSize, i * 5); //Generate a horizontal slice of m
+
+			// Create a thread that runs the threadFunction
+			matrix_multiply_2d_worker(nrows, sliceSize, nslice, sliceSize, mcols, mslice, ret);
+		}	
+	}
+}
+
+double* generate_matrix_slice_vertical(int nrows, int ncols, double* n, int scols, int startCol)
+{
+	int numElements = nrows * scols;
+	double* slice = new double[numElements];
+	for (int i = 0; i < numElements; i++)
+	{
+		//scols = 5
+		// n
+		//  0  1  2  3  4  5  6
+		//  7  8  9 10 11 12 13
+		// 14 15 16 17 18 19 20
+		// 21 22 23 24 25 26 27
+		//slice
+		//  0  1  2  3  4  0  0
+		//  5  6  7  8  9  0  0
+		// 10 11 12 13 14  0  0
+		// 15 16 17 18 19  0  0
+
+		int nrow = i / scols; //First ncols index should be 0, next ncols 1, etc
+		int ncol = i % scols; //0,1,2,3,4,0,1,2,3,4 if scols = 5
+		*(slice + i) = *(n + (nrow * ncols) + ncol + startCol); //Start col is just a shift to the right on each index
+	}
+	return slice;
+}
+
+double* generate_matrix_slice_horizontal(int mrows, int mcols, double* m, int srows, int startRow)
+{
+	int numElements = mcols * srows;
+	double* slice = new double[numElements];
+	for (int i = 0; i < numElements; i++)
+	{
+		//scols = 5
+		// n
+		//  0  1  2  3  4  5  6
+		//  7  8  9 10 11 12 13
+		// 14 15 16 17 18 19 20
+		// 21 22 23 24 25 26 27
+		// 28 29 30 31 32 33 34
+		// 35 36 37 38 39 40 41
+		//slice
+		//  0  1  2  3  4  5  6
+		//  7  8  9 10 11 12 13
+		// 14 15 16 17 18 19 20
+		// 21 22 23 24 25 26 27
+		// 28 29 30 31 32 33 34
+		//  0  0  0  0  0  0  0
+
+		//This is just gathering, in direct order, numElements elements and putting them in the slice
+		*(slice + i) = *(m + (startRow * mcols) + i);
+	}
+	return slice;
+}
+
+double* matrix_multiply_2d_worker(int nrows, int ncols, double* n, int mrows, int mcols, double* m, double* ret)
+{
+	if (ncols != mrows) return 0;
+
+	int finalNumRows = nrows;
+	int finalNumCols = mcols;
+	int numElements = finalNumRows * finalNumCols;
 	if (numElements == 0) return 0;
 
 	switch (ncols) //Handle these cases (its faster if we catch)
@@ -18,10 +109,10 @@ UINT_32* matrix_multiply_2d(int nrows, int ncols , UINT_32* n, int mrows, int mc
 		{
 			int nrow = i / mcols; //First ncols index should be 0, next ncols 1, etc
 			int mcol = i % mcols; //I.E. 0,1,2,0,1,2 if mcols is 3
-			*(ret + i) = *(n + (nrow * ncols)) * *(m + mcol); //Just multiply the indices together
+			*(ret + i) += *(n + (nrow * ncols)) * *(m + mcol); //Just multiply the indices together
 		}
 		break;
-	case 2:                   
+	case 2:
 		// [2 2] * [ 1 ] = [ 2*1 + 2*1 ]
 		//         [ 1 ]   [ 2*1 + 2*1 ]
 		for (int i = 0; i < numElements; i++)
@@ -30,13 +121,13 @@ UINT_32* matrix_multiply_2d(int nrows, int ncols , UINT_32* n, int mrows, int mc
 			int mcol = i % mcols; //I.E. 0,1,2,0,1,2 if mcols is 3
 			//These make this code much more readable
 			//Hoping the compiler optimizes them out of the actual code though
-			UINT_32* n1 = (n + (nrow * ncols));
-			UINT_32* n2 = (n1 + 1);
+			double* n1 = (n + (nrow * ncols));
+			double* n2 = (n1 + 1);
 
-			UINT_32* m1 = (m + mcol);
-			UINT_32* m2 = (m1 + mcols);
+			double* m1 = (m + mcol);
+			double* m2 = (m1 + mcols);
 
-			*(ret + i) = ((*n1 * *m1) + (*n2 * *m2));
+			*(ret + i) += ((*n1 * *m1) + (*n2 * *m2));
 		}
 		break;
 	case 3:
@@ -47,16 +138,16 @@ UINT_32* matrix_multiply_2d(int nrows, int ncols , UINT_32* n, int mrows, int mc
 		{
 			int nrow = i / mcols; //First ncols index should be 0, next ncols 1, etc
 			int mcol = i % mcols; //I.E. 0,1,2,0,1,2 if mcols is 3
-			UINT_32* n1 = (n + (nrow * ncols));
-			UINT_32* n2 = (n1 + 1);
-			UINT_32* n3 = (n2 + 1);
+			double* n1 = (n + (nrow * ncols));
+			double* n2 = (n1 + 1);
+			double* n3 = (n2 + 1);
 
-			UINT_32* m1 = (m + mcol);
-			UINT_32* m2 = (m1 + mcols);
-			UINT_32* m3 = (m2 + mcols);
+			double* m1 = (m + mcol);
+			double* m2 = (m1 + mcols);
+			double* m3 = (m2 + mcols);
 
-			*(ret + i) = ((*n1 * *m1) + (*n2 * *m2) + (*n3 * *m3));
-			printf("%d * %d + %d * %d + %d * %d = %d\n", *n1, *m1, *n2, *m2, *n3, *m3,  *(ret + i));
+			*(ret + i) += ((*n1 * *m1) + (*n2 * *m2) + (*n3 * *m3));
+			//printf("%d * %d + %d * %d + %d * %d = %d\n", *n1, *m1, *n2, *m2, *n3, *m3, *(ret + i));
 		}
 		break;
 	case 4:
@@ -64,18 +155,18 @@ UINT_32* matrix_multiply_2d(int nrows, int ncols , UINT_32* n, int mrows, int mc
 		{
 			int nrow = i / mcols; //First ncols index should be 0, next ncols 1, etc
 			int mcol = i % mcols; //I.E. 0,1,2,0,1,2 if mcols is 3
-			UINT_32* n1 = (n + (nrow * ncols));
-			UINT_32* n2 = (n1 + 1);
-			UINT_32* n3 = (n2 + 1);
-			UINT_32* n4 = (n3 + 1);
+			double* n1 = (n + (nrow * ncols));
+			double* n2 = (n1 + 1);
+			double* n3 = (n2 + 1);
+			double* n4 = (n3 + 1);
 
-			UINT_32* m1 = (m + mcol);
-			UINT_32* m2 = (m1 + mcols);
-			UINT_32* m3 = (m2 + mcols);
-			UINT_32* m4 = (m3 + mcols);
+			double* m1 = (m + mcol);
+			double* m2 = (m1 + mcols);
+			double* m3 = (m2 + mcols);
+			double* m4 = (m3 + mcols);
 
-			*(ret + i) = ((*n1 * *m1) + (*n2 * *m2) + (*n3 * *m3) + (*n4 * *m4));
-			printf("%d * %d + %d * %d + %d * %d + %d * %d = %d\n", *n1, *m1, *n2, *m2, *n3, *m3, *n4, *m4, *(ret + i));
+			*(ret + i) += ((*n1 * *m1) + (*n2 * *m2) + (*n3 * *m3) + (*n4 * *m4));
+			//printf("%d * %d + %d * %d + %d * %d + %d * %d = %d\n", *n1, *m1, *n2, *m2, *n3, *m3, *n4, *m4, *(ret + i));
 		}
 		break;
 	case 5:
@@ -83,40 +174,32 @@ UINT_32* matrix_multiply_2d(int nrows, int ncols , UINT_32* n, int mrows, int mc
 		{
 			int nrow = i / mcols; //First ncols index should be 0, next ncols 1, etc
 			int mcol = i % mcols; //I.E. 0,1,2,0,1,2 if mcols is 3
-			UINT_32* n1 = (n + (nrow * ncols));
-			UINT_32* n2 = (n1 + 1);
-			UINT_32* n3 = (n2 + 1);
-			UINT_32* n4 = (n3 + 1);
-			UINT_32* n5 = (n4 + 1);
+			double* n1 = (n + (nrow * ncols));
+			double* n2 = (n1 + 1);
+			double* n3 = (n2 + 1);
+			double* n4 = (n3 + 1);
+			double* n5 = (n4 + 1);
 
-			UINT_32* m1 = (m + mcol);
-			UINT_32* m2 = (m1 + mcols);
-			UINT_32* m3 = (m2 + mcols);
-			UINT_32* m4 = (m3 + mcols);
-			UINT_32* m5 = (m4 + mcols);
+			double* m1 = (m + mcol);
+			double* m2 = (m1 + mcols);
+			double* m3 = (m2 + mcols);
+			double* m4 = (m3 + mcols);
+			double* m5 = (m4 + mcols);
 
-			*(ret + i) = ((*n1 * *m1) + (*n2 * *m2) + (*n3 * *m3) + (*n4 * *m4) + (*n5 * *m5));
+			*(ret + i) += ((*n1 * *m1) + (*n2 * *m2) + (*n3 * *m3) + (*n4 * *m4) + (*n5 * *m5));
 		}
-		
+
 		break;
 	default:
-		if (numElements > 10000)
+		for (int i = 0; i < numElements; i++)
 		{
-
-		}
-		else //Single threaded case
-		{
-			for (int i = 0; i < numElements; i++)
+			int nrow = i / mcols; //First ncols index should be 0, next ncols 1, etc
+			int mcol = i % mcols; //I.E. 0,1,2,0,1,2 if mcols is 3
+			for (int j = 0; j < ncols; j++) //The extra loop makes it woefully slower but it works for all cases, so oh well
 			{
-				*(ret + i) = 0;
-				int nrow = i / mcols; //First ncols index should be 0, next ncols 1, etc
-				int mcol = i % mcols; //I.E. 0,1,2,0,1,2 if mcols is 3
-				for (int j = 0; j < ncols; j++) //The extra loop makes it woefully slower but it works for all cases, so oh well
-				{
-					UINT_32* nj = (n + (nrow * ncols) + j);
-					UINT_32* mj = (m + mcol + (mcols * j));
-					*(ret + i) += *nj * *mj;
-				}
+				double* nj = (n + (nrow * ncols) + j);
+				double* mj = (m + mcol + (mcols * j));
+				*(ret + i) += *nj * *mj;
 			}
 		}
 		break;
@@ -124,21 +207,3 @@ UINT_32* matrix_multiply_2d(int nrows, int ncols , UINT_32* n, int mrows, int mc
 	return ret;
 }
 
-void multithreaded(int start, int stop, int mcols, int ncols, UINT_32* ret, UINT_32* n, UINT_32* m)
-{
-	for (int i = start; i < stop; i++)
-	{
-		for (int i = 0; i < stop; i++)
-		{
-			*(ret + i) = 0;
-			int nrow = i / mcols; //First ncols index should be 0, next ncols 1, etc
-			int mcol = i % mcols; //I.E. 0,1,2,0,1,2 if mcols is 3
-			for (int j = 0; j < ncols; j++) //The extra loop makes it woefully slower but it works for all cases, so oh well
-			{
-				UINT_32* nj = (n + (nrow * ncols) + j);
-				UINT_32* mj = (m + mcol + (mcols * j));
-				*(ret + i) += *nj * *mj;
-			}
-		}
-	}
-}
--- MatrixLibrary/MatrixLibrary.h
+++ MatrixLibrary/MatrixLibrary.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "iostream"
+#include <thread>
 
 #ifdef MATRIX_LIBRARY_EXPORTS
 #define MATRIX_LIBRARY_API __declspec(dllexport)
@@ -8,4 +9,4 @@
 #endif
 #define UINT_32 unsigned int
 
-extern "C" MATRIX_LIBRARY_API UINT_32 * matrix_multiply_2d(int n1, int n2, UINT_32 * n, int m1, int m2, UINT_32 * m);
+extern "C" MATRIX_LIBRARY_API double * matrix_multiply_2d(int n1, int n2, double* n, int m1, int m2, double* m);
